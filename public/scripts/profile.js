9985f24121c8bfe271edf30a2c3e4205a3c4bc8f
Connor Mattox <mattoxcw@mattoxcw-5510.rose-hulman.edu>
2019-10-29 01:50:59 -0400

    Finished first draft of profile edit

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -14,12 +14,32 @@ rh.enableTextFields = function () {
 	const rhEmail = new mdc.textField.MDCTextField(document.querySelector('.rhEmail'));
 	const curPos = new mdc.textField.MDCTextField(document.querySelector('.curPos'));
 	const tkNum = new mdc.textField.MDCTextField(document.querySelector('.tkNum'));
+	const curMajor = new mdc.textField.MDCTextField(document.querySelector('.curMajor'));
+	const gradYear = new mdc.textField.MDCTextField(document.querySelector('.gradYear'));
+	const nonRhEmail = new mdc.textField.MDCTextField(document.querySelector('.nonRhEmail'));
+	const hAddr = new mdc.textField.MDCTextField(document.querySelector('.hAddr'));
+	const state = new mdc.textField.MDCTextField(document.querySelector('.state'));
+	const city = new mdc.textField.MDCTextField(document.querySelector('.city'));
+	const zipCode = new mdc.textField.MDCTextField(document.querySelector('.zipCode'));
+	const bDay = new mdc.textField.MDCTextField(document.querySelector('.bDay'));
+	const pNum = new mdc.textField.MDCTextField(document.querySelector('.pNum'));
+	const tSize = new mdc.textField.MDCTextField(document.querySelector('.tSize'));
 
 	const nfName = new mdc.notchedOutline.MDCNotchedOutline(document.querySelector('.nfName'));
 	const nlName = new mdc.notchedOutline.MDCNotchedOutline(document.querySelector('.nlName'));
 	const nrhEmail = new mdc.notchedOutline.MDCNotchedOutline(document.querySelector('.nrhEmail'));
 	const ncurPos = new mdc.notchedOutline.MDCNotchedOutline(document.querySelector('.ncurPos'));
 	const ntkNum = new mdc.notchedOutline.MDCNotchedOutline(document.querySelector('.ntkNum'));
+	const ncurMajor = new mdc.notchedOutline.MDCNotchedOutline(document.querySelector('.ncurMajor'));
+	const ngradYear = new mdc.notchedOutline.MDCNotchedOutline(document.querySelector('.ngradYear'));
+	const nnonRhEmail = new mdc.notchedOutline.MDCNotchedOutline(document.querySelector('.nnonRhEmail'));
+	const nhAddr = new mdc.notchedOutline.MDCNotchedOutline(document.querySelector('.nhAddr'));
+	const nstate = new mdc.notchedOutline.MDCNotchedOutline(document.querySelector('.nstate'));
+	const ncity = new mdc.notchedOutline.MDCNotchedOutline(document.querySelector('.ncity'));
+	const nzipCode = new mdc.notchedOutline.MDCNotchedOutline(document.querySelector('.nzipCode'));
+	const nbDay = new mdc.notchedOutline.MDCNotchedOutline(document.querySelector('.nbDay'));
+	const npNum = new mdc.notchedOutline.MDCNotchedOutline(document.querySelector('.npNum'));
+	const ntSize = new mdc.notchedOutline.MDCNotchedOutline(document.querySelector('.ntSize'));
 }
 
 $(document).ready(() => {
