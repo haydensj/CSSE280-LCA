4c615f07206ca98dc96a8e92edce65395b48dbdc
Connor Mattox <mattoxcw@mattoxcw-5510.rose-hulman.edu>
2019-10-29 01:01:49 -0400

    Completed style for inputs

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -12,10 +12,14 @@ rh.enableTextFields = function () {
 	const fName = new mdc.textField.MDCTextField(document.querySelector('.fName'));
 	const lName = new mdc.textField.MDCTextField(document.querySelector('.lName'));
 	const rhEmail = new mdc.textField.MDCTextField(document.querySelector('.rhEmail'));
-	
+	const curPos = new mdc.textField.MDCTextField(document.querySelector('.curPos'));
+	const tkNum = new mdc.textField.MDCTextField(document.querySelector('.tkNum'));
+
 	const nfName = new mdc.notchedOutline.MDCNotchedOutline(document.querySelector('.nfName'));
 	const nlName = new mdc.notchedOutline.MDCNotchedOutline(document.querySelector('.nlName'));
 	const nrhEmail = new mdc.notchedOutline.MDCNotchedOutline(document.querySelector('.nrhEmail'));
+	const ncurPos = new mdc.notchedOutline.MDCNotchedOutline(document.querySelector('.ncurPos'));
+	const ntkNum = new mdc.notchedOutline.MDCNotchedOutline(document.querySelector('.ntkNum'));
 }
 
 /* Main */
