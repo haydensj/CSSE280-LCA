23638fcbb1a2d08fbd4aed4d8606aa8fcae443a4
Connor Mattox <mattoxcw@mattoxcw-5510.rose-hulman.edu>
2019-11-15 15:41:49 -0500

    Basic layout for logger. Adjusted css for main

--- public/scripts/thetaHourLogger.js
+++ public/scripts/thetaHourLogger.js
@@ -8,10 +8,23 @@
 /** namespace. */
 var rh = rh || {};
 
+rh.enableTextFields = function () {
+	const selectorNames = [
+		"fName",
+		"lName",
+		"cHours",
+		"lHours"
+	];
+	selectorNames.forEach((selectorName) => {
+		new mdc.textField.MDCTextField(document.querySelector("." + selectorName));
+		new mdc.notchedOutline.MDCNotchedOutline(document.querySelector("#n" + selectorName));
+	});
+}
+
 
 $(document).ready(() => {
 	console.log("Ready");
 	rh.initialize((params) => {
-		
+		rh.enableTextFields();
 	});
 });
